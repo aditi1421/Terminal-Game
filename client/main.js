735a47efe85515845846ba0b5bfb7a0821685683
Aditi <iiaditii24@gmail.com>
2025-03-27 14:10:00 -0700

    Full project commit: client and server

---
+++ client/main.js
@@ -0,0 +1,28 @@
+const term = new Terminal();
+term.open(document.getElementById('terminal'));
+
+const socket = new WebSocket(`ws://${location.host}`);
+
+let inputBuffer = '';
+
+term.onData(data => {
+  // If user pressed Enter
+  if (data === '\r') {
+    socket.send(inputBuffer);
+    term.writeln(''); // move to next line
+    inputBuffer = '';
+  } else if (data === '\u007F') {
+    // Handle Backspace
+    if (inputBuffer.length > 0) {
+      inputBuffer = inputBuffer.slice(0, -1);
+      term.write('\b \b'); // visually remove character
+    }
+  } else {
+    inputBuffer += data;
+    term.write(data); // Local echo
+  }
+});
+
+socket.onmessage = function (event) {
+  term.writeln(event.data); // Display server messages
+};
